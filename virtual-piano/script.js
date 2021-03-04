ad31d1db8f4177647fc0d32c5efb9fa70b309844
SiarhKul <SiarhKul@gmail.com>
2021-03-04 21:31:36 +0300

    feat: create playing music by many keys

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -2,53 +2,24 @@ const piano = document.querySelector('.piano')
 const btnLetters = document.querySelector('.btn-letters')
 const btnNotes = document.querySelector('.btn-notes')
 const pianoKey = document.querySelectorAll('.piano-key')
-let isPressed = false
 
 //------------------------------------------------mouse
 piano.addEventListener('mousedown', (e) => {
-  const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);
-  e.target.classList.add('piano-key-active')
-  audio.currentTime = 0;
-  audio.play();
-
+  playMusic(e)
+  pianoKey.forEach(key => key.addEventListener('mouseover', playMusic))
+  pianoKey.forEach(key => key.addEventListener('mouseout', removeActiveKey))
 })
 
 piano.addEventListener('mouseup', (e) => {
-  e.target.classList.remove('piano-key-active')
-
+  pianoKey.forEach(key => key.removeEventListener('mouseover', playMusic))
+  removeActiveKey(e)
 })
-
-window.document.onmousedown = function (e) {
-  isPressed = true
-
-  if (isPressed) {
-    piano.addEventListener('mouseover', () => {
-      const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);
-      e.target.classList.add('piano-key-active')
-      audio.currentTime = 0;
-      audio.play();
-    })
-
-    piano.addEventListener('mouseout', () => {
-      e.target.classList.remove('piano-key-active')
-    })
-    console.log('Pressed');
-  }
-}
-
-window.document.onmouseup = function (e) {
-  isPressed = false
-  console.log('NO Pressed');
-}
 
 //------------------------------------------------keyboard
 window.addEventListener("keydown", (e) => {
   const key = document.querySelector(`.piano-key[data-letter="${e.key.toUpperCase()}"]`);
   if (key === null) return;
   const audio = document.querySelector(`audio[data-note="${key.dataset.note}"]`);
-  // const keyNote = key.getAttribute("data-note");
-  // key.classList.add("playing");
-  // note.innerHTML = keyNote;
   audio.currentTime = 0;
   audio.play();
 });
@@ -79,6 +50,15 @@ document.querySelector('.fullscreen').addEventListener('click', () => {
   }
 })
 
+const playMusic = (e) => {
+  const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);
+  e.target.classList.add('piano-key-active')
+  audio.currentTime = 0;
+  audio.play();
+}
+const removeActiveKey = (e) => {
+  e.target.classList.remove('piano-key-active')
+}
 const turnOffFullscreen = () => {
   document.addEventListener("keypress", function (e) {
     if (e.key === 'Escape') {
